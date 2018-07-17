9792da9e121cc335dd49b62fb1563a58818026b9
Carl Dawson <carl@invisible-gorilla.com>
2018-07-17 19:19:04 -0400

    Add Users class and updateUserList event

---
+++ server/utils/users.js
@@ -0,0 +1,45 @@
+[{
+  id: '/213123123123',
+  name: 'Carl',
+  room: 'The Office Fans'
+}]
+
+// addUser(id, name, room)
+// removeUser(id)
+// getUser(id)
+// getUserList(room)
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+
+  addUser (id, name, room) {
+    let user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+
+  removeUser (id) {
+    let user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter(user => user.id !== id);
+    }
+
+    return user;
+  }
+
+  getUser (id) {
+    return this.users.filter(user => user.id === id)[0];
+  }
+  
+  getUserList (room) {
+    let users = this.users.filter(user => user.room === room);
+    let namesArray = users.map(user => user.name);
+
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
